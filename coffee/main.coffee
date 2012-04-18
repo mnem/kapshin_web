dda5e0c3b3f9806e230c77ffd2ed2a0897794f0d
David Wagner <mnem@noiseandheat.com>
2012-04-18 22:39:34 +0100

    Hacky workingness

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,10 +1,23 @@
 KAPSHIN_SERVICE = 'http://kapshin.noiseandheat.com/kap'
+OLD_SOURCE = ''
 
 generate_kapshin = (style, url, message) ->
     url = encodeURIComponent url
     message = encodeURIComponent message
     kapshin_url = "#{KAPSHIN_SERVICE}/#{style}/#{url}/#{message}.png"
+    $("#kapshin-image").attr("src", OLD_SOURCE)
     $("#kapshin-image").attr("src", kapshin_url)
 
-$(document).ready ->
+kapshin = =>
     generate_kapshin "lol", $("#image-url").val(), $("#image-message").val()
+
+$(document).ready ->
+    OLD_SOURCE = $("#kapshin-image").attr("src")
+
+    $("#image-url").blur -> kapshin()
+    $("#image-message").blur -> kapshin()
+
+    $("#image-url").keypress (event) -> kapshin() if event.charCode == 13
+    $("#image-message").keypress (event) -> kapshin() if event.charCode == 13
+
+    kapshin()
--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,39 @@
 (function() {
-  var KAPSHIN_SERVICE, generate_kapshin;
+  var KAPSHIN_SERVICE, OLD_SOURCE, generate_kapshin, kapshin,
+    _this = this;
 
   KAPSHIN_SERVICE = 'http://kapshin.noiseandheat.com/kap';
+
+  OLD_SOURCE = '';
 
   generate_kapshin = function(style, url, message) {
     var kapshin_url;
     url = encodeURIComponent(url);
     message = encodeURIComponent(message);
     kapshin_url = "" + KAPSHIN_SERVICE + "/" + style + "/" + url + "/" + message + ".png";
+    $("#kapshin-image").attr("src", OLD_SOURCE);
     return $("#kapshin-image").attr("src", kapshin_url);
   };
 
-  $(document).ready(function() {
+  kapshin = function() {
     return generate_kapshin("lol", $("#image-url").val(), $("#image-message").val());
+  };
+
+  $(document).ready(function() {
+    OLD_SOURCE = $("#kapshin-image").attr("src");
+    $("#image-url").blur(function() {
+      return kapshin();
+    });
+    $("#image-message").blur(function() {
+      return kapshin();
+    });
+    $("#image-url").keypress(function(event) {
+      if (event.charCode === 13) return kapshin();
+    });
+    $("#image-message").keypress(function(event) {
+      if (event.charCode === 13) return kapshin();
+    });
+    return kapshin();
   });
 
 }).call(this);
